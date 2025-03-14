c25e25d00467daff9d0e36a5e6ddecb4d9da3c88
ignaciodz <ignaciodiazbr@gmail.com>
2025-03-13 22:34:09 -0500

    add: Agregar clase de CreateProduct

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,24 @@
-export class CreateProductDto {}
+import { IsBoolean, IsNumber, IsOptional, IsString, Min } from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    nombre: string;
+  
+    @IsString()
+    categoria: string;
+  
+    @IsNumber()
+    @Min(0)
+    precio: number;
+  
+    @IsNumber()
+    @Min(1)
+    cantidad: number;
+  
+    @IsString()
+    unidad: string; // kg, litros, unidades, etc.
+  
+    @IsBoolean()
+    @IsOptional() // Es opcional porque tiene un valor por defecto en el schema
+    comprado?: boolean;
+}
